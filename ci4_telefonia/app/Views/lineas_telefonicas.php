cd86db606230e48e3537afde32b8c08a40d961a7
Mani-9 <manu201119@hotmail.com>
2024-07-23 12:07:20 -0600

    23/07/2024/2

--- ci4_telefonia/app/Views/lineas_telefonicas.php
+++ ci4_telefonia/app/Views/lineas_telefonicas.php
@@ -8,8 +8,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="../../public/css/diseno.css">
-  <link rel="stylesheet" href="../public/css/diseno.css">
+
+  <link rel="stylesheet" href="<?= base_url('css/diseno.css')?>">
 </head>
 
 <body>
